be2821091828debcfbcaaa810f359cbbcb3b621e
juthomas <juthomas@student.42.fr>
2023-02-05 14:01:31 +0100

    [ADD] Glitched Title base

--- website_src/src/components/GlitchedTitle/GlitchedTitle.tsx
+++ website_src/src/components/GlitchedTitle/GlitchedTitle.tsx
@@ -1,5 +1,20 @@
-import './index.css'
+import "./index.css";
 
-export const GlitchedTitle = (): JSX.Element => {
-  return <div style={{backgroundColor: 'red'}}>Title</div>;
+export interface GlitchedTitleProps {}
+
+export const GlitchedTitle = ({}: GlitchedTitleProps): JSX.Element => {
+  return (
+    <div style={{ position: "relative" }}>
+      <div className="title1">
+        <h1 className="glitch1 title before">AS-SIMT</h1>
+        <h1 className="glitch1 title">AS-SIMT</h1>
+        <h1 className="glitch1 title after">AS-SIMT</h1>
+      </div>
+      <div className="title2">
+        <h1 className="glitch2 title before">السمت</h1>
+        <h1 className="glitch2 title">السمت</h1>
+        <h1 className="glitch2 title after">السمت</h1>
+      </div>
+    </div>
+  );
 };
