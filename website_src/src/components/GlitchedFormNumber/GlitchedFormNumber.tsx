df6eb8aae088360bc857cd6bbc24e5e89d7ea5f6
juthomas <juthomas@student.42.fr>
2023-05-05 16:26:56 +0200

    [ADD] Some things

--- website_src/src/components/GlitchedFormNumber/GlitchedFormNumber.tsx
+++ website_src/src/components/GlitchedFormNumber/GlitchedFormNumber.tsx
@@ -1,15 +1,15 @@
 import { FormEvent, useState } from "react";
 import { GlitchedButton } from "../GlitchedButton";
-import { GlitchedInput } from "../GlitchedInput";
+import { GlitchedInputNumber } from "../GlitchedInputNumber/GlitchedInputNumber";
 
 interface GlitchedFormProps {
   label?: string;
   placeholder?: string;
-  value?:string;
+  value?:number;
 }
 
-export const GlitchedForm = ({ label = 'default', placeholder=label, value }: GlitchedFormProps): JSX.Element => {
-  const [inputState, setInputState] = useState(value || "");
+export const GlitchedFormNumber = ({ label = 'default', placeholder=label, value }: GlitchedFormProps): JSX.Element => {
+  const [inputState, setInputState] = useState(value || 0);
 
   const submitFunction = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,7 +18,7 @@ export const GlitchedForm = ({ label = 'default', placeholder=label, value }: Gl
 
   return (
     <form onSubmit={submitFunction} style={{ color: "red" }}>
-      <GlitchedInput
+      <GlitchedInputNumber
         inputState={inputState}
         setInputState={setInputState}
         label={label}
